fed16df84dd0c27d4401e917e2880cd15c1a6a14
tnm3009 <86880497+tnm3009@users.noreply.github.com>
2021-07-03 20:34:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bae à!',
-    introDesc: `Đến với anh em ôm hi vọng còn mưu mẹo thì quên đem
-    Đếch cần 36 kế em chỉ cần kế bên anh<33`,
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không,em bị điên à?',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Còn bây giờ thì vào ib em và book lịch đi chơi nàooo<33333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com'messages/t/100028294520692
+    messLink: 'http://m.me/nminh.309
 }
